8a29ded58008e55b65f0a1248c988db4629250ea
Nimesh Neema <nimeshneema@gmail.com>
2025-01-28 19:17:15 +0530

    Fixed spellings.

--- Code/main_loop.c
+++ Code/main_loop.c
@@ -1,4 +1,4 @@
-    // A C prorgam demonstrating how we can interchange the working mechanism between a counter based vs a condition based loop.
+    // A C program demonstrating how we can interchange the working mechanism between a counter based vs a condition based loop.
     // The two looping techniques can be interchanged easily.
     // But we prefer going with the one which "reads" more naturally as a prose.
 
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[]) {
     char name[] = "William";
-    int length_of_name = 0; // The entity length_of_name is a variable and it is possible to acidentally and inadvertantly change to an incorrect value. But C programming lanaguge does not have a device/mechanism to make constants.
+    int length_of_name = 0; // The entity length_of_name is a variable and it is possible to accidentally and inadvertently change to an incorrect value. But C programming language does not have a device/mechanism to make constants.
 
     char *character_pointer = name; // the bare name of an array in C is the address of the first element, which is the same thing as the address of the array.
 
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
         length_of_name++;
     }
 
-    const int length_of_string = length_of_name;    // A more scalable approach. The source code adapts to any change in the contents of the `name` characer array variable.
+    const int length_of_string = length_of_name;    // A more scalable approach. The source code adapts to any change in the contents of the `name` character array variable.
 
         // Reset the pointer
     character_pointer = name;
